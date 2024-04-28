971cb03dbaab3af78019e8b943cbab7476895704
Rafael E. Cortez <rafael.ecortez@gmail.com>
2024-04-28 02:58:22 -0300

    Corrigiendo fechas

--- scripts/data/datosCanalesTV.js
+++ scripts/data/datosCanalesTV.js
@@ -1,43 +1,43 @@
 const canalesTV = [
     new CanalTV(
         "Nickelodeon",
-        new Date("1979"),
+        new Date(1979, 0, 1),
         null,
         "Canal infantil conocido por programas animados y series en vivo, destacando por su " +
         "diversidad de contenidos familiares y entretenidos para niños y adolescentes.",
         new Imagen("../medios/images/logo_nickelodeon.png", "Logo de Nickelodeon")),
     new CanalTV(
         "Cartoon Network",
-        new Date("1992"),
+        new Date(1992, 0, 1),
         null,
         "Canal de animación con una mezcla de series originales, famoso por su variedad de personajes " +
         "icónicos y humor creativo dirigido adiferentes edades.",
         new Imagen("../medios/images/logo_cartoon_network.png", "Logo de Cartoon Network")),
     new CanalTV(
         "Disney Channel",
-        new Date("1983"),
+        new Date(1983, 0, 1),
         null,
         "Canal familiar de Disney con programas, películas y series originales, dirigido a niños y " +
         "preadolescentes. Ofrece entretenimiento y valores familiares a través de narrativas encantadoras.",
         new Imagen("../medios/images/logo_disney_channel.png", "Logo de Disney Channel")),
     new CanalTV(
         "Jetix",
-        new Date("2004"),
-        new Date("2010"),
+        new Date(2004, 0, 1),
+        new Date(2010, 0, 1),
         "Antiguo bloque de programación transformado en Disney XD, canal centrado en la acción y " +
         "animación, atrayendo a adolescentes con una combinación de series animadas y live-action emocionantes.",
         new Imagen("../medios/images/logo_jettix.PNG", "Logo de Jetix")),
     new CanalTV(
         "Discovery Kids",
-        new Date("1996"),
+        new Date(1996, 0, 1),
         null,
         "Canal educativo para niños, conocido por programas educativos y aventuras animadas que fomentan " +
         "la curiosidad y el aprendizaje mediante experiencias entretenidas e informativas.",
         new Imagen("../medios/images/logo_discovery_kids.png", "Logo de Discovery Kids")),
     new CanalTV(
         "Fox Kids",
-        new Date("1990"),
-        new Date("2002"),
+        new Date(1990, 0, 1),
+        new Date(2002, 0, 1),
         "Canal de televisión infantil conocido por su programación de dibujos animados y series de " +
         "acción, ofreciendo entretenimiento para niños y preadolescentes con una variedad de contenido.",
         new Imagen("../medios/images/logo_fox_kids.PNG", "Logo de Fox Kids")
